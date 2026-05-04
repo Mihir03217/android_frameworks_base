5af94bc449091303093f1a35d8f2e9dfee9d643c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-05-04 16:55:18 +0000

    fixup! AppBindArgs: infrastructure for passing extra args to app process init
    Pass AppBindArgs directly from system_server to app process instead of making the AppBindArgs query
    from inside the app process.
    
    This approach is faster (no extra Binder IPC) and allows to easily support isolated processes.

--- services/core/java/com/android/server/ext/PackageManagerHooks.java
+++ services/core/java/com/android/server/ext/PackageManagerHooks.java
@@ -1,9 +1,9 @@
 package com.android.server.ext;
 
-import android.Manifest;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
 import android.app.AppBindArgs;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.GosPackageState;
 import android.content.pm.GosPackageStateFlag;
@@ -11,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManagerInternal;
 import android.os.Binder;
 import android.os.Build;
+import android.ext.PackageId;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.util.ArraySet;
@@ -18,6 +19,7 @@ import android.util.ArraySet;
 import com.android.server.pm.Computer;
 import com.android.server.pm.GosPackageStatePmHooks;
 import com.android.server.pm.PackageManagerService;
+import com.android.server.pm.ext.PackageExt;
 import com.android.server.pm.ext.PackageHooks;
 import com.android.server.pm.permission.Permission;
 import com.android.server.pm.permission.SpecialRuntimePermUtils;
@@ -45,13 +47,23 @@ public class PackageManagerHooks {
     }
 
     @Nullable
-    public static Bundle getExtraAppBindArgs(PackageManagerService pm, String packageName) {
-        final int callingUid = Binder.getCallingUid();
-        final int callingPid = Binder.getCallingPid();
-        final int appId = UserHandle.getAppId(callingUid);
-        final int userId = UserHandle.getUserId(callingUid);
+    public static Bundle getExtraAppBindArgs(Context context, PackageManagerInternal pm,
+                                             String packageName, int appUid, int pid) {
+        if (android.os.Flags.isDevBuild()) {
+            Slog.d("AppBindArgs", "obtaining args for pkgName " + packageName
+                    + ", appUid " + appUid + ", pid " + pid);
+        }
 
-        Computer pmComputer = pm.snapshotComputer();
+        // Note that:
+        // - app UID differs from process UID for isolated processes
+        // - for android:externalService processes (e.g. WebView processes), app UID and package
+        // name values are client's, not host's
+
+        final int appId = UserHandle.getAppId(appUid);
+        final int userId = UserHandle.getUserId(appUid);
+
+        Computer pmComputer = (Computer) pm.snapshot();
+
         PackageStateInternal pkgState = pmComputer.getPackageStateInternal(packageName);
         if (pkgState == null) {
             return null;
